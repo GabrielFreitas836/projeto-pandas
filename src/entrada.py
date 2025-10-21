8498204d25b0cd2cd5df495963472b15232e12fc
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-10-21 19:38:45 -0300

    feat: configurar parcialmente a classe Entrada de entrada.py

--- src/entrada.py
+++ src/entrada.py
@@ -4,3 +4,10 @@
     data: 21/10/2025
     descrição: leitura e validação de arquivos
 """
+
+import pandas as pd
+
+class Entrada:
+    def __init__(self, caminho_arquivo: str):
+        self.caminho_arquivo = caminho_arquivo
+        
