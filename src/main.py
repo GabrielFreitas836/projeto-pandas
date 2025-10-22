ab222d424332c0f0fe61fed5df8a4bd349e642f2
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-10-21 22:02:05 -0300

    feat: configurar main.py

--- src/main.py
+++ src/main.py
@@ -4,3 +4,13 @@
     data: 20/10/2025
     descrição: entrada principal da execução do programa
 """
+
+from entrada import Entrada
+
+if __name__ == "__main__":
+    try:
+        c = Entrada()
+        c.menu()
+    except Exception as error:
+        print("Erro: ", error)
+        
