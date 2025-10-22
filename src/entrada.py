1de6d70f518f3180304bb5e1c85dae51b90985da
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-10-21 22:01:20 -0300

    feat: configuração inicial das funções menu e ler_excel

--- src/entrada.py
+++ src/entrada.py
@@ -6,8 +6,57 @@
 """
 
 import pandas as pd
+import time
 
 class Entrada:
-    def __init__(self, caminho_arquivo: str):
-        self.caminho_arquivo = caminho_arquivo
-        
+    def menu(self):
+        while True:
+            print("=" *50, "\n")
+            print("Olá, seja bem-vindo!\n")
+            print("=" *50, "\n")
+
+            time.sleep(0.8)
+
+            print("[1] - Ler arquivo de Excel\n[2] - Ler arquivo de banco MySQL\n[3] - Sair do programa\n")
+            opcao = int(input("Escolha uma das opções acima: "))
+
+            match opcao:
+                case 1:
+                    arquivo_excel = self.ler_excel()
+
+                    if arquivo_excel:
+                        break
+                    else:
+                        continue
+                case 2:
+                    self.ler_mysql()
+                case 3:
+                    print("Saindo do programa...")
+                    time.sleep(0.8)
+                    break
+    
+    def ler_excel(self):
+        try:
+            print("=" *50)
+            caminho_arquivo = input("Digite um caminho de arquivo válido: ")
+            frame = pd.read_excel(caminho_arquivo)
+            print("Procurando arquivo...")
+            time.sleep(0.5)
+
+            if not frame.empty:
+                print("=" *50)
+                print("Exibindo arquivo encontrado...")
+                time.sleep(0.5)
+                print(frame.head())
+                return True
+            else:
+                print("Arquivo vazio! Escolha outro arquivo...")
+                time.sleep(0.8)
+                return False
+                
+                
+        except Exception as error:
+            print("Erro de execução: ", error)
+
+    def ler_mysql():
+        pass
