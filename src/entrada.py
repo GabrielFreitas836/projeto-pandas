e2d51bcdfcb6775fb42cb843fa1bdb76c28740bf
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-10-30 19:46:15 -0300

    feat: adição de loop while, a fim de permitir entrada do nome de tabela válido quando valor anterior for inválido

--- src/entrada.py
+++ src/entrada.py
@@ -37,40 +37,44 @@ class Entrada:
                         continue
                 case 2:
                     conn = self.ler_mysql()
+                    while True:
+                        if conn is not None:
+                            cursor = conn.cursor()
+                            cursor.execute("""
+                                SELECT TABLE_NAME 
+                                FROM INFORMATION_SCHEMA.TABLES 
+                                WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = %s
+                            """, (conn.database,))
+                            tables = [t[0] for t in cursor.fetchall()]
 
-                    if conn is not None:
-                        cursor = conn.cursor()
-                        cursor.execute("""
-                            SELECT TABLE_NAME 
-                            FROM INFORMATION_SCHEMA.TABLES 
-                            WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = %s
-                        """, (conn.database,))
-                        tables = [t[0] for t in cursor.fetchall()]
+                            print(tabulate([[t] for t in tables], headers = ["Tabelas"], tablefmt = "fancy_grid"))
+                            time.sleep(0.3)
+                            print("\n")
+                            table_name = input(f"Escolha uma das tabelas de {conn.database}: ")
+                            time.sleep(0.3)
+                            print("Procurando arquivo...")
+                            time.sleep(0.8)
 
-                        print(tabulate([[t] for t in tables], headers = ["Tabelas"], tablefmt = "fancy_grid"))
-                        time.sleep(0.3)
-                        print("\n")
+                            if table_name in tables:
+                                query = f"SELECT * FROM `{table_name}`"
+                                df_table = pd.read_sql(query, conn)
 
-                        table_name = input(f"Escolha uma das tabelas de {conn.database}: ")
-                        time.sleep(0.8)
-                        print("Exibindo arquivo...")
-
-                        if table_name in tables:
-                            query = f"SELECT * FROM `{table_name}`"
-                            df_table = pd.read_sql(query, conn)
-
-                            if df_table.empty:
-                                time.sleep(0.3)
-                                print("Tabela vazia")
-                                break
+                                if df_table.empty:
+                                    time.sleep(0.3)
+                                    print("Tabela vazia")
+                                    break
+                                else:
+                                    print("Exibindo arquivo...")
+                                    print(tabulate(df_table.head(), headers = 'keys', tablefmt = "fancy_grid"))
+                                    break
                             else:
-                                print(tabulate(df_table.head(), headers = 'keys', tablefmt = "fancy_grid"))
-                                break
-                        else:
-                            print("Tabela não encontrada!")
+                                print("Tabela não encontrada! Tente novamente!")
+                                time.sleep(0.8)
+                                continue
 
-                        cursor.close()
-                        conn.close()
+                    cursor.close()
+                    conn.close()
+                    break
                 case 3:
                     print("Saindo do programa...")
                     time.sleep(0.8)
